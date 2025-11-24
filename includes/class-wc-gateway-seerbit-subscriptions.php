46fa5e413530428c97cb77f60149dc21c696f0f1
Chiukwumezie Stanley <ukadikestanley@gmail.com>
2025-11-24 16:33:51 +0100

    Debugging admin settings

---
+++ includes/class-wc-gateway-seerbit-subscriptions.php
@@ -0,0 +1,19 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Class WC_Gateway_Seerbit_Subscriptions
+ */
+class WC_Gateway_Seerbit_Subscriptions extends WC_Gateway_Seerbit {
+
+    /**
+	 * Constructor
+	 */
+	public function __construct() {
+        parent::__construct();
+    }
+
+}
