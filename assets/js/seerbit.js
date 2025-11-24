f226b085b90a101694a4bd8f5d8c484ea3fb12a0
Chiukwumezie Stanley <ukadikestanley@gmail.com>
2025-11-24 20:24:34 +0100

    Updating Seerbit Inline Payment

--- assets/js/seerbit.js
+++ assets/js/seerbit.js
@@ -14,7 +14,68 @@ jQuery( function( $ ) {
 	} );
 
     function wcSeerbitFormHandler() {
-        console.log(wc_seerbit_params);
+        
+        $( '#wc-seerbit-form' ).hide();
+
+		if ( seerbit_submit ) {
+			seerbit_submit = false;
+			return true;
+		}
+
+		let $form = $( 'form#payment-form, form#order_review' ),
+			seerbit_tranref = $form.find( 'input.seerbit_tranref' ),
+			split_code = '';
+            tokenize = false;
+
+        seerbit_tranref.val('');
+        
+        let amount = Number( wc_seerbit_params.amount );
+
+        let seerbit_callback = function( transaction ) {
+            $form.append( '<input type="hidden" class="seerbit_tranref" name="seerbit_tranref" value="' + 'tranref-placeholder' + '"/>' );
+            seerbit_submit = true;
+    
+            $form.submit();
+    
+            $( 'body' ).block( {
+                message: null,
+                overlayCSS: {
+                    background: '#fff',
+                    opacity: 0.6
+                },
+                css: {
+                    cursor: "wait"
+                }
+            } );
+        };
+    
+        let paymentData = {
+            public_key: wc_seerbit_params.public_key,
+            email: wc_seerbit_params.email,
+            currency: wc_seerbit_params.currency,
+            tranref: wc_seerbit_params.tranref,
+            amount: amount,
+            description: wc_seerbit_params.public_key,
+            full_name: wc_seerbit_params.public_key,
+            customization: {
+                confetti: false
+            }
+        }
+
+        if(wc_seerbit_params.split_code){
+            paymentData['splitCode'] = wc_seerbit_params.split_code;
+        }
+
+        if(wc_seerbit_params.tokenize){
+            paymentData['tokenize'] = wc_seerbit_params.tokenize;
+        }
+
+        if(wc_seerbit_params.payment_methods){
+            paymentData.customization['payment_method'] = wc_seerbit_params.wc_seerbit_params.payment_methods;
+        }
+
+        console.log(paymentData);
+
     }
 
 });
