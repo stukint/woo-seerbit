038abb5892d9a422f39f339112c8bc2fb5cc11a7
Chiukwumezie Stanley <ukadikestanley@gmail.com>
2025-11-27 16:32:59 +0100

    Update transaction verification

--- includes/class-wc-gateway-seerbit.php
+++ includes/class-wc-gateway-seerbit.php
@@ -1079,7 +1079,7 @@ class WC_Gateway_Seerbit extends WC_Payment_Gateway_CC {
 
 			if($seerbit_response !== false){
 				
-				if(strtolower($seerbit_response->status) == 'success'){
+				if(strtolower($seerbit_response->data->message) == 'successful' && $seerbit_response->data->code == '00'){
 					$order_details = explode('_', $seerbit_response->data->payments->paymentReference);
 					$order_id = (int) $order_details[1];
 					$order = wc_get_order( $order_id );
