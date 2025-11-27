e1de89c16866e7563eea6f891a6fd7fae4390c7d
Chiukwumezie Stanley <ukadikestanley@gmail.com>
2025-11-27 20:08:09 +0100

    Updating Webhook

--- includes/class-wc-gateway-seerbit.php
+++ includes/class-wc-gateway-seerbit.php
@@ -1203,7 +1203,7 @@ class WC_Gateway_Seerbit extends WC_Payment_Gateway_CC {
 
 		error_log(print_r($_SERVER['HTTP_X_SEERBIT_SIGNATURE'], true));
 
-		error_log(print_r( hash_hmac( 'sha512', $json, $this->secret_key ), true));
+		error_log(print_r( hash_hmac( 'sha512', $json, $this->secret_key.$this->public_key ), true));
 
 		// validate event do all at once to avoid timing attack.
 		// if ( $_SERVER['HTTP_X_PAYSTACK_SIGNATURE'] !== hash_hmac( 'sha512', $json, $this->secret_key ) ) {
