455fbbc89fa2415ba62fac233b88c5a0be7e34c2
Chiukwumezie Stanley <ukadikestanley@gmail.com>
2026-02-18 20:52:29 +0100

    refactor redirect payment 5

--- includes/class-wc-gateway-seerbit.php
+++ includes/class-wc-gateway-seerbit.php
@@ -734,13 +734,12 @@ class WC_Gateway_Seerbit extends WC_Payment_Gateway_CC {
 
 		$request = wp_remote_post( $seerbit_url, $args );
 
-		error_log(print_r($seerbit_params, true));
-
 
 		if ( ! is_wp_error( $request ) && 200 === wp_remote_retrieve_response_code( $request ) ) {
 
 			$seerbit_response = json_decode( wp_remote_retrieve_body( $request ) );
 
+			error_log(print_r($seerbit_response, true));
 			wp_redirect( $seerbit_response->data->payments->redirectLink );
 			exit;
 			
